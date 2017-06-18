f13a67eb643e9baa7fab569395dacecda9981281
Matheus Link <matheus.guimaraes@4all.com>
2017-06-18 15:40:10 -0300

    espaçamento entre widgets

--- src/components/WidgetsList/index.js
+++ src/components/WidgetsList/index.js
@@ -28,26 +28,26 @@ class WidgetsList extends React.Component {
   render() {
     console.log('state', this.state.data);
     return (
-      <div className="list">        
-        <div className="wid1">
+      <div className="list">
+        <div className="wid wid1">
           <div className="corblue"><div className="icon"><FaShoppingBag className="iconsize"/></div></div>
           <div className="fundoWidget">
             <Widgets name="Comments" number={this.state.data.comments}/>
           </div>
         </div>
-        <div className="wid2">
+        <div className="wid wid2">
           <div className="coryellow"><div className="icon"><FaCommentO className="iconsize"/></div></div>
           <div className="fundoWidget">
           <Widgets name="New Orders" number={this.state.data.newOrders}/>
           </div>
         </div>
-        <div className="wid3">
+        <div className="wid wid3">
           <div className="corgreen"><div className="icon"><FaGroup className="iconsize"/></div></div>
           <div className="fundoWidget">
           <Widgets name="NewUsers" number={this.state.data.newUsers}/>
         </div>
         </div>
-        <div className="wid4">
+        <div className="wid wid4">
           <div className="corred"><div className="icon"><FaTasks className="iconsize"/></div></div>
           <div className="fundoWidget">
           <Widgets name="Page Views" number={this.state.data.pageViews}/>
